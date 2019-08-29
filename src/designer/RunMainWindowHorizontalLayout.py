badebf04ac4c5d52333e15fa9bb95779a0b23896
swj <2318085206@qq.com>
2019-08-29 17:07:51 +0800

    first gui app

---
+++ src/designer/RunMainWindowHorizontalLayout.py
@@ -0,0 +1,12 @@
+import sys
+import MainWindowHorizontalLayout
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWin = QMainWindow()
+    ui = MainWindowHorizontalLayout.Ui_MainWindow()
+    ui.setupUi(MainWin)
+    MainWin.show()
+    sys.exit(app.exec_())
