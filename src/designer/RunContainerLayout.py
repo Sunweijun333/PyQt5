78624c5aaa85071e7cc87e3013ab1e5697fc1a1c
swj <2318085206@qq.com>
2019-08-30 09:08:51 +0800

    first gui app

---
+++ src/designer/RunContainerLayout.py
@@ -0,0 +1,12 @@
+import sys
+import ContainerLayout
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWin = QMainWindow()
+    ui = ContainerLayout.Ui_MainWindow()
+    ui.setupUi(MainWin)
+    MainWin.show()
+    sys.exit(app.exec_())
