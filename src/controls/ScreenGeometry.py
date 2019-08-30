6661e67654b139aea85fd46de87e88f4a5b14eb5
swj <2318085206@qq.com>
2019-08-30 15:24:16 +0800

    first gui app

--- src/controls/ScreenGeometry.py
+++ src/controls/ScreenGeometry.py
@@ -5,20 +5,20 @@ from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QPushButton,
 
 def onclick_button():
     print "1"
-    print "widget.x() = ", widget.x()
-    print "widget.y() = ", widget.y()
-    print "widget.width() = ", widget.width()
-    print "widget.height() = ", widget.height()
+    print "widget.x() = ", widget.x()    # 250 (窗口横坐标)
+    print "widget.y() = ", widget.y()    # 200 (窗口纵坐标)
+    print "widget.width() = ", widget.width()    # 300 (工作区宽度)
+    print "widget.height() = ", widget.height()  # 240 (工作区高度)
     print "2:不包含标题栏"
-    print "widget.geometry().x() = ", widget.geometry().x()
-    print "widget.geometry().y() = ", widget.geometry().y()
-    print "widget.geometry().width() = ", widget.geometry().width()
-    print "widget.geometry().height() = ", widget.geometry().height()
+    print "widget.geometry().x() = ", widget.geometry().x()    # 250 (工作区横坐标)
+    print "widget.geometry().y() = ", widget.geometry().y()    # 228 (工作区纵坐标)
+    print "widget.geometry().width() = ", widget.geometry().width()     # 300 (工作区宽度)
+    print "widget.geometry().height() = ", widget.geometry().height()   # 240 (工作区高度)
     print "3:高度包含标题栏"
-    print "widget.frameGeometry().x() = ", widget.frameGeometry().x()
-    print "widget.frameGeometry().y() = ", widget.frameGeometry().y()
-    print "widget.frameGeometry().width() = ", widget.frameGeometry().width()
-    print "widget.frameGeometry().height() = ", widget.frameGeometry().height()
+    print "widget.frameGeometry().x() = ", widget.frameGeometry().x()   # 250 (窗口横坐标)
+    print "widget.frameGeometry().y() = ", widget.frameGeometry().y()   # 200 (窗口纵坐标)
+    print "widget.frameGeometry().width() = ", widget.frameGeometry().width()   # 300 (窗口宽度)
+    print "widget.frameGeometry().height() = ", widget.frameGeometry().height() # 268 (窗口高度)
 
 
 app = QApplication(sys.argv)
